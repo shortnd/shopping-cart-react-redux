4273f058988881db9a45ddf066a81397d5f67795
Collin OConnell <ococncol@gmail.com>
2017-03-30 10:49:50 -0400

    init commit and done

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../stylesheets/App.css';
+import Cart from '../containers/Cart'
+
+class App extends Component {
+  render() {
+    return (
+      <Cart />
+    );
+  }
+}
+
+export default App;
